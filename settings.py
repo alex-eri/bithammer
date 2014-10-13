c1d2a87825e7f09f936af01a117b234dc026b317
Aleksandr Eerie <alex-eri@ya.ru>
2014-10-13 23:06:21 +0400

    ssh feature, mac

--- settings.py
+++ settings.py
@@ -1,9 +1,14 @@
 ARP_POISON = False
 SSH_BAN = True
+
+#keywords are {ip},{port},{mac}
 BAN_RULE = [
-    '/ip firewall filter add chain=forward action=drop dst-address={ip} dst-port={port} protocol=tcp',
-    '/ip firewall filter add chain=forward action=drop dst-address={ip} dst-port={port} protocol=udp',
+    '/ip firewall filter add chain=forward action=drop comment=bithammer dst-address={ip} dst-port={port} protocol=tcp',
+    '/ip firewall filter add chain=forward action=drop comment=bithammer dst-address={ip} dst-port={port} protocol=udp',
     ]
 
 LOGIN = 'admin'
 PASSWORD = 'secret'
+#None if gateway is same with hosts gateway
+GATEWAY = None
+
