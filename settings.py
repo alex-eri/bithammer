7e1a8341c6d2df173324df4e8eccca877daab180
Aleksandr Eerie <alex-eri@ya.ru>
2014-10-13 23:42:12 +0400

    ssh feature, fixes, not tested

--- settings.py
+++ settings.py
@@ -1,10 +1,16 @@
 ARP_POISON = False
 SSH_BAN = True
 
-#keywords are {ip},{port},{mac}
-BAN_RULE = [
-    '/ip firewall filter add chain=forward action=drop comment=bithammer dst-address={ip} dst-port={port} protocol=tcp',
-    '/ip firewall filter add chain=forward action=drop comment=bithammer dst-address={ip} dst-port={port} protocol=udp',
+# keywords are {ip},{port},{mac}
+# must check existance before insert!
+# mikrotik sample
+BAN_RULES = [
+'''if ([:len [/ip firewall filter find protocol="tcp" dst-port="{port}" dst-address="{ip}"]] = 0) else={put "exist"} do={
+/ip firewall filter add chain=forward action=drop comment=bithammer dst-address="{ip}" dst-port="{port}" protocol="tcp"
+}''',
+'''if ([:len [/ip firewall filter find protocol="udp" dst-port="{port}" dst-address="{ip}"]] = 0) else={put "exist"} do={
+/ip firewall filter add chain=forward action=drop comment=bithammer dst-address="{ip}" dst-port="{port}" protocol="udp"
+}''',
     ]
 
 LOGIN = 'admin'
