9b10d3b7a17e1869d7ce5f42500d9c6d9ee24b7b
Aleksandr Eerie <alex-eri@ya.ru>
2014-10-14 02:38:01 +0400

    ssh feature

--- settings.py
+++ settings.py
@@ -5,16 +5,17 @@ SSH_BAN = True
 # must check existance before insert!
 # mikrotik sample
 BAN_RULES = [
-'''if ([:len [/ip firewall filter find protocol="tcp" dst-port="{port}" dst-address="{ip}"]] = 0) else={put "exist"} do={
-/ip firewall filter add chain=forward action=drop comment=bithammer dst-address="{ip}" dst-port="{port}" protocol="tcp"
-}''',
-'''if ([:len [/ip firewall filter find protocol="udp" dst-port="{port}" dst-address="{ip}"]] = 0) else={put "exist"} do={
-/ip firewall filter add chain=forward action=drop comment=bithammer dst-address="{ip}" dst-port="{port}" protocol="udp"
-}''',
+#'''if ([:len [/ip firewall filter find protocol="tcp" dst-port="{port}" dst-address="{ip}"]] = 0) else={put "exist"} do={
+#/ip firewall filter add chain=forward action=drop comment=bithammer dst-address="{ip}" dst-port="{port}" protocol="tcp"
+#}''',
+#'''if ([:len [/ip firewall filter find protocol="udp" dst-port="{port}" dst-address="{ip}"]] = 0) else={put "exist"} do={
+#/ip firewall filter add chain=forward action=drop comment=bithammer dst-address="{ip}" dst-port="{port}" protocol="udp"
+#}''',
+'''/log warning "{ip},{port},{mac}" '''
     ]
 
 LOGIN = 'admin'
 PASSWORD = 'secret'
 #None if gateway is same with hosts gateway
 GATEWAY = None
-
+NEED_MAC = False
